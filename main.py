026bcced7fd82ecea337476db63b12bf9559a36f
gjacobs1992 <gjacobs1992@gmail.com>
2025-06-08 16:09:48 -0700

    starting asteroids class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -8,10 +9,29 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    updatable.add(player)
+    drawable.add(player)
+    
 
     while True:
-          screen.fill((0, 0, 0))
-          pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for sprite in drawable:
+             sprite.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0
 
 if __name__ == "__main__":
         main()
